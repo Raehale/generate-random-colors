f3014eedfb29e95934e3712723cb9a12bf1ed716
Raehale <87107302+Raehale@users.noreply.github.com>
2024-08-16 12:01:05 -0600

    creating six color generators

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const colorsEl = document.getElementById('colors');
+
+
+
 let color = '#000000'.replace('0',function(){
   return (~~(Math.random()*16)).toString(16).padStart(6, '0')
 });
@@ -14,14 +18,39 @@ function* generator() {
 
 const hexGen = generator();
 
+changeColors();
+
 document.getElementById('nextColorButton').addEventListener('click', () => {
-/*
-Challenge:
-    2. When the "Next Color" button is clicked, update 
-       the textContent and backgroundColor attributes below.
-*/ 
-        const color = hexGen.next().value;
-        console.log(color)
-        document.getElementById('colorText').textContent = color
-        document.getElementById('colorDisplay').style.backgroundColor = color
+  changeColors();
+});
+
+function changeColors(){
+  const colorOne = hexGen.next().value;
+  const colorTwo = hexGen.next().value;
+  const colorThree = hexGen.next().value;
+  const colorFour = hexGen.next().value;
+  const colorFive = hexGen.next().value;
+  const colorSix = hexGen.next().value;
+
+  document.getElementById('color1Text').textContent = colorOne;
+  document.getElementById('color1Display').style.backgroundColor = colorOne;
+
+  document.getElementById('color2Text').textContent = colorTwo;
+  document.getElementById('color2Display').style.backgroundColor = colorTwo;
+
+  document.getElementById('color3Text').textContent = colorThree;
+  document.getElementById('color3Display').style.backgroundColor = colorThree;
+
+  document.getElementById('color4Text').textContent = colorFour;
+  document.getElementById('color4Display').style.backgroundColor = colorFour;
+
+  document.getElementById('color5Text').textContent = colorFive;
+  document.getElementById('color5Display').style.backgroundColor = colorFive;
+
+  document.getElementById('color6Text').textContent = colorSix;
+  document.getElementById('color6Display').style.backgroundColor = colorSix;
+}
+
+colorsEl.addEventListener('dblclick', (event) => {
+  console.log(event.target.id)
 })
