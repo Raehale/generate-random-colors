52cd3b17c62ed91300bb05d95e011548a266cdda
Raehale <87107302+Raehale@users.noreply.github.com>
2024-08-18 18:36:35 -0600

    styling, mobility, organizing js, locking colors, copying hex, no duplicates

--- index.js
+++ index.js
@@ -1,56 +1,107 @@
 const colorsEl = document.getElementById('colors');
+const hexGen = colorgenerator();
+const copyNotifEl = document.getElementById('copiedNotif');
 
+/**Generates colors on page load */
+generateColors();
 
-
-let color = '#000000'.replace('0',function(){
-  return (~~(Math.random()*16)).toString(16).padStart(6, '0')
+/**When new colors button is clicked, new colors are generated */
+document.getElementById('newColorsButton').addEventListener('click', () => {
+	generateColors();
 });
 
-function* generator() {
-  while (true) {
-    let color = '#000000'.replace(/0/g,function(){
-      return (~~(Math.random()*16)).toString(16)
-    });
-    yield color;
-  }
-  
-}
-
-const hexGen = generator();
-
-changeColors();
-
-document.getElementById('nextColorButton').addEventListener('click', () => {
-  changeColors();
-});
-
-function changeColors(){
-  const colorOne = hexGen.next().value;
-  const colorTwo = hexGen.next().value;
-  const colorThree = hexGen.next().value;
-  const colorFour = hexGen.next().value;
-  const colorFive = hexGen.next().value;
-  const colorSix = hexGen.next().value;
-
-  document.getElementById('color1Text').textContent = colorOne;
-  document.getElementById('color1Display').style.backgroundColor = colorOne;
-
-  document.getElementById('color2Text').textContent = colorTwo;
-  document.getElementById('color2Display').style.backgroundColor = colorTwo;
-
-  document.getElementById('color3Text').textContent = colorThree;
-  document.getElementById('color3Display').style.backgroundColor = colorThree;
-
-  document.getElementById('color4Text').textContent = colorFour;
-  document.getElementById('color4Display').style.backgroundColor = colorFour;
-
-  document.getElementById('color5Text').textContent = colorFive;
-  document.getElementById('color5Display').style.backgroundColor = colorFive;
-
-  document.getElementById('color6Text').textContent = colorSix;
-  document.getElementById('color6Display').style.backgroundColor = colorSix;
-}
-
-colorsEl.addEventListener('dblclick', (event) => {
-  console.log(event.target.id)
+/**when the lock icon is clicked, the color will not change even when new colors are generated */
+colorsEl.addEventListener('click', (event) => {
+	if (event.target.getAttribute('data-id') == 'lock') {
+		lockColor(event.target.id);
+	}
 })
+
+/**When a color or its hex code is double clicked, the color is copied to the users dash and an alert is given */
+colorsEl.addEventListener('dblclick', (event) => {
+	if (event.target.dataset.color) {
+		copyColor(event.target.dataset.color)
+	}
+})
+
+/**A generator function generating random hex codes */
+function* colorgenerator() {
+	while (true) {
+		let color = '#000000'.replace(/0/g,function(){
+			return (~~(Math.random()*16)).toString(16)
+		});
+		yield color;
+	}
+}
+
+/**Getting random hex codes and displaying them */
+function generateColors() {
+	const colorOne = hexGen.next().value;
+	const colorTwo = hexGen.next().value;
+	const colorThree = hexGen.next().value;
+	const colorFour = hexGen.next().value;
+	const colorFive = hexGen.next().value;
+	const colorSix = hexGen.next().value;
+
+	displayColors('1', colorOne);
+	displayColors('2', colorTwo);
+	displayColors('3', colorThree);
+	displayColors('4', colorFour);
+	displayColors('5', colorFive);
+	displayColors('6', colorSix);
+}
+
+/**Displaying the colors if the color isn't locked and isn't already used */
+function displayColors(number, color){
+	const colorText = document.getElementById(`color${number}Text`);
+	const colorDisplay = document.getElementById(`color${number}Display`);
+	if (colorText.parentElement.dataset.lock === 'false'){
+
+		let colorDisplayArr = document.getElementsByClassName('color-display');
+		let allowedColor = true;
+		for (let i = 0; i < colorDisplayArr.length; i++){
+			let colorDisplayAttr = colorDisplayArr[i].dataset.color;
+			if (colorDisplayAttr === color){
+				return allowedColor = false;
+			}
+		}
+
+		if (allowedColor) {
+			colorText.parentElement.dataset.color = `${color}`;
+			colorText.dataset.color = `${color}`;
+			colorDisplay.dataset.color = `${color}`;
+	
+			colorText.textContent = color;
+			colorDisplay.style.backgroundColor = color;
+		} else {
+			displayColors(number, hexGen.next().value);
+		}
+
+	}
+}
+
+/**Turning the lock icon unlocked/locked and changing the lock dataset */
+function lockColor(colorId){
+	const selectedColorEl = document.getElementById(colorId);
+
+	selectedColorEl.classList.toggle('fa-unlock');
+	selectedColorEl.classList.toggle('fa-lock');
+
+	if (selectedColorEl.classList.contains('fa-lock')){
+		selectedColorEl.parentElement.dataset.lock = 'true';
+	} else if (selectedColorEl.classList.contains('fa-unlock')){
+		selectedColorEl.parentElement.dataset.lock = 'false';
+	}
+}
+
+/**copies color and gives alert */
+function copyColor(colorHex){
+	navigator.clipboard.writeText(colorHex);
+
+	copyNotifEl.innerHTML = `Successfully copied the hex code ${colorHex} to clipboard!`;
+	copyNotifEl.style.display = 'flex';
+
+	setTimeout(() => {
+		copyNotifEl.style.display = 'none';
+	}, 5000)
+}
